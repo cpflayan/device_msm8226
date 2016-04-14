45d0e946138b2731b00ce09449942eb5b0229819
cpflayan <cpflayan@gmail.com>
2016-04-14 08:09:33 +0800

    first commit

---
+++ BoardConfig.mk
@@ -0,0 +1,114 @@
+# config.mk
+#
+# Product-specific compile-time definitions.
+#
+
+ifeq ($(TARGET_ARCH),)
+TARGET_ARCH := arm
+endif
+
+BOARD_USES_GENERIC_AUDIO := true
+USE_CAMERA_STUB := false
+
+TARGET_KERNEL_APPEND_DTB := true
+
+# Compile with msm kernel
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_HAS_QC_KERNEL_SOURCE := true
+
+-include $(QCPATH)/common/msm8226/BoardConfigVendor.mk
+
+#TODO: Fix-me: Setting TARGET_HAVE_HDMI_OUT to false
+# to get rid of compilation error.
+TARGET_HAVE_HDMI_OUT := false
+TARGET_USES_OVERLAY := true
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+TARGET_NO_BOOTLOADER := false
+TARGET_NO_KERNEL := false
+TARGET_NO_RADIOIMAGE := true
+TARGET_NO_RPC := true
+
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_CPU_ABI  := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := krait
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+TARGET_HARDWARE_3D := false
+TARGET_BOARD_PLATFORM := msm8226
+TARGET_BOOTLOADER_BOARD_NAME := MSM8226
+
+BOARD_KERNEL_BASE        := 0x00000000
+BOARD_KERNEL_PAGESIZE    := 2048
+BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
+BOARD_RAMDISK_OFFSET     := 0x02000000
+
+# Enables Adreno RS driver
+#OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+
+#TARGET_BOOTIMG_SIGNED := true
+
+# Shader cache config options
+# Maximum size of the  GLES Shaders that can be cached for reuse.
+# Increase the size if shaders of size greater than 12KB are used.
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+
+# Maximum GLES shader cache size for each app to store the compiled shader
+# binaries. Decrease the size if RAM or Flash Storage size is a limitation
+# of the device.
+MAX_EGL_CACHE_SIZE := 2048*1024
+
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_PERSISTIMAGE_FILE_SYSTEM_TYPE := ext4
+
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 earlyprintk androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x37 androidboot.bootdevice=msm_sdcc.1
+BOARD_KERNEL_SEPARATED_DT := true
+
+BOARD_EGL_CFG := device/qcom/$(TARGET_BOARD_PLATFORM)/egl.cfg
+
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01000000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01000000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 838860800
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 5066686464
+BOARD_CACHEIMAGE_PARTITION_SIZE := 33554432
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 5242880
+BOARD_TOMBSTONESIMAGE_PARTITION_SIZE := 73400320
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+
+# Enable suspend during charger mode
+BOARD_CHARGER_ENABLE_SUSPEND := true
+
+# Add NON-HLOS files for ota upgrade
+ADD_RADIO_FILES ?= true
+
+# Added to indicate that protobuf-c is supported in this build
+PROTOBUF_SUPPORTED := false
+
+TARGET_USES_ION := true
+TARGET_USE_KINGFISHER_OPTIMIZATION := true
+
+TARGET_INIT_VENDOR_LIB := libinit_msm
+TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_msm
+
+#Add support for firmare upgrade on 8226
+HAVE_FT_FW_UPGRADE := true
+HAVE_SYNAPTICS_I2C_RMI4_FW_UPGRADE := true
+
+TARGET_LDPRELOAD := libNimsWrap.so
+
+#Use dlmalloc instead of jemalloc for mallocs
+MALLOC_IMPL := dlmalloc
+
+#Disable memcpy_base.S optimization
+TARGET_CPU_MEMCPY_BASE_OPT_DISABLE := true
+
+USE_OPENGL_RENDERER := true
+
+FEATURE_QCRIL_UIM_SAP_SERVER_MODE := true
+
+# Control flag between KM versions
+TARGET_HW_KEYMASTER_V03 := true
